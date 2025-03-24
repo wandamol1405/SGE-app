901e06c192ec39bdfbe572c3cb355b3fa0270ee8
wandamol1405 <wanda.molina@mi.unc.edu.ar>
2025-03-24 20:44:37 -0300

    Fix main.jsx

--- frontend/sge-app/src/main.jsx
+++ frontend/sge-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./frontend/sge-app/public/reset.css";
+//import "./frontend/sge-app/public/reset.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { InvoiceProvider } from "./context/InvoiceContext";
