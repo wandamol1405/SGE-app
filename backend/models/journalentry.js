488f94ada109ea84ade70f9a7fc9a5449f2f0a15
wandamol1405 <wanda.molina@mi.unc.edu.ar>
2025-04-12 18:23:01 -0300

    Fix journalentries

--- backend/models/journalentry.js
+++ backend/models/journalentry.js
@@ -36,7 +36,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "JournalEntry",
-      tableName: "Journalentries",
+      tableName: "journalentries",
     }
   );
 
